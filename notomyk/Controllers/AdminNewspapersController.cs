c96b88403a5ec2c3becb6c44104b128327ec8319
KZ <krzysiek.zygadlo@gmail.com>
2018-02-05 23:41:09 +0100

    designRefactoring

--- notomyk/Controllers/AdminNewspapersController.cs
+++ notomyk/Controllers/AdminNewspapersController.cs
@@ -3,9 +3,15 @@ using notomyk.Infrastructure;
 using notomyk.Models;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
+using System.Web.Helpers;
+using System.Web.Hosting;
 using System.Web.Mvc;
+using static notomyk.Controllers.ManageController;
 
 namespace notomyk.Controllers
 {
@@ -24,7 +30,8 @@ namespace notomyk.Controllers
         [Authorize(Roles = "Admin")]
         public ActionResult GetNewspapers()
         {
-            var newspapers = db.Newspaper.Select(n => new {                
+            var newspapers = db.Newspaper.Select(n => new
+            {
                 n.NewspaperName,
                 n.NewspaperLink,
                 n.NewspaperIconLink,
@@ -56,7 +63,7 @@ namespace notomyk.Controllers
             bool status = false;
             if (ModelState.IsValid)
             {
-                if (newspaper.tbl_NewspaperID!= 0)
+                if (newspaper.tbl_NewspaperID != 0)
                 {
                     var n = db.Newspaper.Where(x => x.tbl_NewspaperID == newspaper.tbl_NewspaperID).FirstOrDefault();
 
@@ -82,5 +89,79 @@ namespace notomyk.Controllers
             }
 
         }
+
+        [HttpPost]
+        public ActionResult UploadLogo(HttpPostedFileBase file, int newspaperID)
+        {
+            if (file != null && file.ContentLength > 0)
+            {
+                var photo = new WebImage(file.InputStream);
+                var newspaperName = GetNewspaperName(newspaperID);
+
+                var a = AddPhoto(
+                    photo, 
+                    250, 
+                    ConfigurationManager.AppSettings["NewspapersIconsFolder_250"], 
+                    GetNewspaperName(newspaperID), 
+                    file.FileName,
+                    newspaperName
+                    );         
+                
+                var b = AddPhoto(
+                    photo, 
+                    50, 
+                    ConfigurationManager.AppSettings["NewspapersIconsFolder_ICO"], 
+                    GetNewspaperName(newspaperID), 
+                    file.FileName,
+                    newspaperName
+                    );
+
+                if (a && b) { UpdateLogoName(newspaperID, newspaperName); }
+            }
+            return RedirectToAction("Save", new { id = newspaperID });
+        }
+        public bool AddPhoto(WebImage photo, int size, string path, string newspaperName,string fileName, string logoName)
+        {
+            
+            photo.Resize(width: size, height: size, preserveAspectRatio: true, preventEnlarge: true);
+
+
+            var fileExt = Path.GetExtension(fileName);
+            var fnm = logoName + ".png";
+
+            if (fileExt.ToLower().EndsWith(".png") || fileExt.ToLower().EndsWith(".jpg") || fileExt.ToLower().EndsWith(".gif"))// Important for security if saving in webroot
+            {
+                var filePath = HostingEnvironment.MapPath(path) + fnm;
+                var directory = new DirectoryInfo(HostingEnvironment.MapPath(path));
+                if (directory.Exists == false)
+                {
+                    directory.Create();
+                }
+                ViewBag.FilePath = filePath.ToString();
+
+                photo.Save(filePath, "png", false);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public string GetNewspaperName(int newspaperID)
+        {
+            var newspaper = db.Newspaper.Where(n => n.tbl_NewspaperID == newspaperID).FirstOrDefault();
+            var result = newspaper.NewspaperName.Split('.');
+            return string.Concat("logo_", result[0]);
+        }
+
+        public void UpdateLogoName(int newspaperID, string logoName)
+        {
+            var newspaper = db.Newspaper.Where(n => n.tbl_NewspaperID == newspaperID).FirstOrDefault();
+            newspaper.NewspaperIconLink = string.Concat(logoName, ".png") ;
+            db.SaveChanges();
+        }
     }
+
+
 }
--- notomyk/Scripts/CustomScriptComments.js
+++ notomyk/Scripts/CustomScriptComments.js
@@ -25,7 +25,7 @@
     $(document).on('submit', '.frmAddReply', function (event) {
         event.preventDefault();
         var $notifBox = $(this).closest('.commentBoxJQ');
-        var $comment = $(this).closest('.addReply').find('.message');
+        var $comment = $(this).find('.addReply').find('.message');
         var $newsID = $('.frmNewsID').val();
         var $parentID = $(this).find('.commentID').val();
         var $template = $('#replyPattern').html();
